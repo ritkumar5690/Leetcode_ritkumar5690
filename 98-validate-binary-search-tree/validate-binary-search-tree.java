353eaa545daa553c31a052348f01a81dbeaa1147
ritkumar5690 <riteshdeoria2000@gmail.com>
2025-12-12 23:06:46 +0530

    Time: 2 ms (12.27%) | Memory: 45.5 MB (6.15%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> list = new ArrayList<>();
+    public boolean isValidBST(TreeNode root) {
+        helper(root);
+
+        for(int i =1;i<list.size();i++){
+            if(list.get(i)<= list.get(i-1)) return false;
+        }
+        return true;
+    }
+    private void helper(TreeNode root){
+        if(root == null) return ;
+        
+        helper(root.left);
+        list.add(root.val);
+        helper(root.right);
+         
+    }
+}
