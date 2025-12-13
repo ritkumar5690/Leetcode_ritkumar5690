9f995d49daf3296d9c6269eff93c0e49236661aa
ritkumar5690 <riteshdeoria2000@gmail.com>
2025-12-13 23:18:09 +0530

    Time: 175 ms (11.31%) | Memory: 169.7 MB (5.47%) - LeetSync

--- 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
@@ -16,7 +16,6 @@
 class Solution {
     private TreeSet<Integer> set = new TreeSet<>();
     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
-
         helper(root);
         List<List<Integer>> list= new ArrayList<>();
         for (int q : queries) {
@@ -30,30 +29,7 @@ class Solution {
         }
         return list;
     }
-    // private int binary(int[] arr,int target,boolean flag){
-    //     int star =0,end = arr.length-1;
-    //     int res = -1;
-    //     while(star <= end){
-    //         int mid = star + (end-star)/2;
-    //         if(flag && arr[mid] <= target){
-    //             res = arr[mid];
-    //             star = mid+1;
-    //         } 
-    //         else if(flag && arr[mid] > target){
-    //             end = mid-1;
-    //         }
-    //         if(!flag && arr[mid] < target){
-               
-    //             star = mid+1;
-    //         } 
-    //         else if(!flag && arr[mid] >= target){
-    //             res = arr[mid];
-    //             end = mid-1;
-    //         }
-
-    //     }
-    //     return res;
-    // }
+    
     private void helper(TreeNode root){
         if(root == null) return;
         helper(root.left);
