69214279974e42653029b48fb1d8ca29f1edfa4b
ritkumar5690 <riteshdeoria2000@gmail.com>
2025-12-13 23:10:32 +0530

    Time: 146 ms (12.77%) | Memory: 177.6 MB (5.47%) - LeetSync

---
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeSet<Integer> set = new TreeSet<>();
+    public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+
+        helper(root);
+        List<List<Integer>> list= new ArrayList<>();
+        int[] arr = set.stream().mapToInt(Integer::intValue).toArray();
+        int n = arr.length;
+        for(int i =0;i<queries.size();i++){
+            List<Integer> temp = new ArrayList<>();
+            int target = queries.get(i);
+            if(target >= arr[0]){
+                int elem = binary(arr,target,true);
+                temp.add(elem);
+            }
+            else{
+                temp.add(-1);
+            }
+            if(target <= arr[n-1]){
+                int elem = binary(arr,target,false);
+                temp.add(elem);
+            }
+            else{
+                temp.add(-1);
+            }
+         list.add(temp);
+        }
+        return list;
+    }
+    private int binary(int[] arr,int target,boolean flag){
+        int star =0,end = arr.length-1;
+        int res = -1;
+        while(star <= end){
+            int mid = star + (end-star)/2;
+            if(flag && arr[mid] <= target){
+                res = arr[mid];
+                star = mid+1;
+            } 
+            else if(flag && arr[mid] > target){
+                end = mid-1;
+            }
+            if(!flag && arr[mid] < target){
+               
+                star = mid+1;
+            } 
+            else if(!flag && arr[mid] >= target){
+                res = arr[mid];
+                end = mid-1;
+            }
+
+        }
+        return res;
+    }
+    private void helper(TreeNode root){
+        if(root == null) return;
+        helper(root.left);
+        set.add(root.val);
+        helper(root.right);
+    }
+}
