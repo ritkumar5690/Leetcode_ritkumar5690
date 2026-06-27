9abd7d6f1c1ad76537e73fc4c480446caa853757
ritkumar5690 <ritsharma1008@gmail.com>
2026-06-28 04:22:51 +0530

    Time: 118 ms (23.26%) | Memory: 72.4 MB (5.81%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        int countOne = 0;
+        for(int i=0;i<nums.length;i++){
+            countOne += (nums[i]== 1)? 1:0;
+        }
+        if(countOne %2 ==0)  countOne--;
+        countOne = Math.max(countOne, 0);
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                list.add(nums[i]);
+                set2.add(nums[i]);
+            }
+            set.add(nums[i]);
+        }
+        int res = 1;
+        for(int i=0;i<list.size();i++){
+            int n = list.get(i);
+            if(n==1) continue;
+            int count = 0;
+            while(set2.contains(n)){
+                count += 2;
+                n = n*n;
+            }
+            if(!set.contains(n)){
+                count --;
+            }
+            else{
+                count++;
+            }
+            if(res < count) res = count;
+        }
+        return countOne > res ? countOne: res;
+    }
+}
