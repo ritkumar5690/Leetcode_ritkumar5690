203c52f7fc94cb9ab4ecf63e73496dbb3dfcd5e9
ritkumar5690 <ritsharma1008@gmail.com>
2026-07-07 08:31:49 +0530

    Time: 7 ms (73.66%) | Memory: 48.9 MB (99.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for(int i =0;i<strs.length;i++){
-            if(set.contains(i)) continue;
-            String s = strs[i];
-            List<String> list = new ArrayList<>();
-            list.add(s);
-            for(int j =i+1;j<strs.length;j++){
-                String t = strs[j];
-                if(check(s,t)){
-                    list.add(t);
-                    set.add(j);
-                }
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            String s = sort(str);
+            if(!map.containsKey(s)){
+                map.put(s,new ArrayList());
             }
-            res.add(list);
+            map.get(s).add(str);
         }
-        return res;
+        return new ArrayList(map.values());
     }
-    private boolean check(String s, String t){
-        if(s.length() != t.length()) return false;
-        int freq[] = new int[26];
-        for(int i =0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-        }
-        for(int cnt : freq) {
-            if(cnt !=0) return false;
-        }
-        return true;
+    private String sort(String s){
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
     }
 }
