01d9706b3d7dcacb8fbb7d0be60df6880f95b658
ritkumar5690 <ritsharma1008@gmail.com>
2026-07-08 15:42:41 +0530

    Time: 18 ms (80.00%) | Memory: 129.8 MB (33.33%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int[] sumAndMultiply(String s, int[][] queries) {
+
+        int n = s.length();
+        int res[] = new int[queries.length];
+        long[] pow10 = new long[n + 1];
+        pow10[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+        }
+        int[] idx = new int[n + 1];
+        long[] total = new long[n + 1];
+        long[] val = new long[n + 1];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            int digit = s.charAt(i) - '0';
+
+            if (digit != 0) {
+                cnt++;
+                val[cnt] = (val[cnt - 1] * 10 + digit) % MOD;
+                total[cnt] = total[cnt - 1] + digit;
+            }
+
+            idx[i + 1] = cnt;
+        }
+
+        for (int i = 0; i <queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            int left = idx[l];
+            int right = idx[r + 1];
+
+            if (left == right) {
+                res[i] = 0;
+                continue;
+            }
+            int len = right - left;
+            long number = (val[right] - (val[left] * pow10[len]) % MOD) % MOD;
+
+            if (number < 0)
+                number += MOD;
+
+            long sum = total[right] - total[left];
+            res[i] = (int) ((number * sum) % MOD);
+        }
+        return res;
+    }
+}
