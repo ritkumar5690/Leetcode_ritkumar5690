51542ad8d67dfff6ecb9493b9a7f0f4a595cb864
ritkumar5690 <ritsharma1008@gmail.com>
2026-07-12 14:26:23 +0530

    Time: 29 ms (84.49%) | Memory: 76.9 MB (31.99%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,48 +1,20 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int nums[] = new int[arr.length];
-        if (arr.length == 0)
-            return new int[] {};
+        int nums[] = new int[arr.length]; 
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             nums[i] = arr[i];
         }
-        int ans[] = new int[arr.length];
-        Arrays.sort(arr);
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                list.add(arr[i]);
+        Arrays.sort(nums);
+        int i = 1;
+        for(int ele : nums){
+            if(!map.containsKey(ele)){
+                map.put(ele,i++);
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                ans[i] = map.get(nums[i]);
-                continue;
-            }
-            int j = binary(list, nums[i]);
-            if (nums[i] == list.get(j)) {
-                ans[i] = j + 1;
-                map.put(nums[i], j + 1);
-            }
+        for(int j =0;j<arr.length;j++){
+            arr[j] = map.get(arr[j]);
         }
-        return ans;
-    }
-
-    private int binary(List<Integer> list, int target) {
-        int low = 0;
-        int high = list.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (list.get(mid) == target)
-                return mid;
-            else if (list.get(mid) < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return -1;
+        return arr;
     }
 }
