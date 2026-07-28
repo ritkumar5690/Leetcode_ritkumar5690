9419fa24fd7c8021c933709648ba98d1bcb8b1cb
ritkumar5690 <ritsharma1008@gmail.com>
2026-07-28 11:42:09 +0530

    Time: 16 ms (99.20%) | Memory: 48.2 MB (38.15%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -6,10 +6,20 @@ class Solution {
         if(s.length()%2 != 0){
             str = String.valueOf(s.charAt(mid));
         }
-        char[] ch = s.substring(0,mid).toCharArray();
-        Arrays.sort(ch);
-        String sorted = new String(ch); 
-        String rev = new StringBuilder(sorted).reverse().toString();
-        return sorted + str + rev;
+        int count[] = new int[26]; 
+        for(int i = 0;i<mid;i++){
+            count[s.charAt(i)-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<26;i++){
+            sb.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
+        }
+
+
+        // char[] ch = s.substring(0,mid).toCharArray();
+        // Arrays.sort(ch);
+        // String sorted = new String(ch); 
+        String rev = new StringBuilder(sb).reverse().toString();
+        return sb.toString() + str + rev;
     }
 }
