f3b47a260eca3b78c15c6327da07ac2a4916b9a4
ritkumar5690 <ritsharma1008@gmail.com>
2026-08-02 09:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (48.30%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean stoneGame(int[] nums) {
-        int n = nums.length;
-       int[][] dp = new int[n][n];
+        return true;
+    //     int n = nums.length;
+    //    int[][] dp = new int[n][n];
 
-       for(int[] i : dp){
-        Arrays.fill(i,-1);
-       }
-       if((n & 1) == 0) return true;
-       return maxDiff(dp, 0, n-1,nums)>0;
+    //    for(int[] i : dp){
+    //     Arrays.fill(i,-1);
+    //    }
+    //    if((n & 1) == 0) return true;
+    //    return maxDiff(dp, 0, n-1,nums)>0;
     }
-    private int maxDiff(int[][] dp, int i,int j, int[] nums){
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i == j) return dp[i][j] = nums[i];
+    // private int maxDiff(int[][] dp, int i,int j, int[] nums){
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(i == j) return dp[i][j] = nums[i];
        
-        return  dp[i][j] = Math.max((nums[i] - maxDiff(dp,i+1, j ,nums)) , nums[j] - maxDiff(dp,i,j-1,nums));
-    }
+    //     return  dp[i][j] = Math.max((nums[i] - maxDiff(dp,i+1, j ,nums)) , nums[j] - maxDiff(dp,i,j-1,nums));
+    // }
 }
